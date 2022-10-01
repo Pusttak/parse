cfff8a131f24f9f22baa8f39f8f77b4223ec6e5d
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-10-01 18:34:52 +0200

    add

--- src/data/index.js
+++ src/data/index.js
@@ -4,8 +4,48 @@ import TopNouns from 'data/TopNouns.csv';
 import TopPrepositions from 'data/TopPrepositions.csv';
 import TopPronouns from 'data/TopPronouns.csv';
 import TopVerbs from 'data/TopVerbs.csv';
+import AnimalsAndNature from 'data/AnimalsAndNature.csv';
+import Characteristics from 'data/Characteristics.csv';
+import EventsAndPlaces from 'data/EventsAndPlaces.csv';
+import Food from 'data/Food.csv';
+import Home from 'data/Home.csv';
+import Person from 'data/Person.csv';
+import TimeAndNumbers from 'data/TimeAndNumbers.csv';
+import VerbsAndLifestyle from 'data/VerbsAndLifestyle.csv';
 
 export const dataList = [
+  {
+    name: 'Characteristics',
+    link: Characteristics,
+  },
+  {
+    name: 'Events and places',
+    link: EventsAndPlaces,
+  },
+  {
+    name: 'Food',
+    link: Food,
+  },
+  {
+    name: 'Home',
+    link: Home,
+  },
+  {
+    name: 'Person',
+    link: Person,
+  },
+  {
+    name: 'Time and numbers',
+    link: TimeAndNumbers,
+  },
+  {
+    name: 'Verbs and lifestyle',
+    link: VerbsAndLifestyle,
+  },
+  {
+    name: 'Animals and nature',
+    link: AnimalsAndNature,
+  },
   {
     name: 'Top Adjectives',
     link: TopAdjectives,
